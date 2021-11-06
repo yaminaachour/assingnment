58eabb896fa8be62ff26863f28b4ff388db8f060
yaminaachour95 <90883710+yaminaachour@users.noreply.github.com>
2021-11-06 15:58:43 +0100

    Delete module4 directory

--- module4/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-((target) => {
-	const byeSpeaker = new Object();
-	const speakWord = 'Good Bye';
-
-	byeSpeaker.speak = (name) => console.log(`${speakWord} ${name}`);
-
-	target.byeSpeaker = byeSpeaker;
-})(window);
