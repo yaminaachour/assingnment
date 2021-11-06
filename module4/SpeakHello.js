981c0b1e34f5894060aff68d1cb8a3060952a802
yaminaachour95 <90883710+yaminaachour@users.noreply.github.com>
2021-11-06 16:02:15 +0100

    Delete module4 directory

--- module4/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-((target) => {
-	const helloSpeaker = new Object();
-	const speakWord = 'Hello';
-
-	helloSpeaker.speak = (name) => console.log(`${speakWord} ${name}`);
-
-	target.helloSpeaker = helloSpeaker;
-})(window);
